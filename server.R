ce3ce991e7e70ede4e5a5e0765df54cac97a965e
Tom McMahon <thmcmahon@gmail.com>
2015-12-26 09:43:24 -0500

    Added presentation code

--- server.R
+++ server.R
@@ -1,13 +1,14 @@
 
 # This is the server logic for a Shiny web application.
 # You can find out more about building applications with Shiny here:
-# 
+#
 # http://www.rstudio.com/shiny/
 #
 
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(lubridate)
 
 pollution <- readRDS('data/china_pm25.Rds') %>%
   filter(qc_name == "Valid", pm25 >= -5)
@@ -21,24 +22,26 @@ shinyServer(function(input, output) {
       group_by(year, month) %>%
       summarise(mean = mean(pm25), max = max(pm25), min = min(pm25),
                 median = median(as.numeric(pm25)))
-    
+
     # Pretty up the output
     monthly$mean <- round(monthly$mean, digits = 2)
     monthly$month <- month(monthly$month, label = TRUE)
     monthly
   })
-  
+
   output$plot <- renderPlot({
-    daily <- pollution %>% 
+    daily <- pollution %>%
       filter(city == input$city) %>%
       group_by(year, month, day) %>%
       summarise(count = n(), mean = mean(pm25))
-    
+
     daily$date <- with(daily, ymd(paste(year, month, day, sep = "-")))
-    
+
     plot_title <- paste("Daily Average PM2.5 levels for", input$city)
     qplot(date, mean, data = daily, geom = c("point", "smooth"),
-          xlab = "Date", ylab = "Average PM2.5", main = plot_title) + 
-      theme_bw()
+          xlab = "Date", ylab = "Average PM2.5", main = plot_title,
+          alpha = 0.5) +
+      theme_bw() +
+      theme(legend.position = "none")
   })
 })
